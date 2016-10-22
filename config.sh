1eae7a394a74bb5165644ab5b0e32ecba60d8dfc
Eric D Cady <eric-cady@users.noreply.github.com>
2016-10-21 20:57:50 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-echo "Hi Eric from GitHub"
+echo
+echo "Hi from GitHub -Eric"
 
 #let's get the current version of Raspbian
 current_ver=`uname -a`
 echo
 echo "you're running:"
-echo "$current_ver"
+echo "  $current_ver"
 echo
-echo "Test 1"
+
+#get some date and time info
+echo "the current time zone set is:"
+echo "  "
+
+#ask for a new password
+echo
+echo "You will now be asked to enter a new password for the pi user"
+passwd
 echo
