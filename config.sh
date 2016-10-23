e7871b2a3b295ea64152f932661f0dc113b16a4a
Eric D Cady <eric-cady@users.noreply.github.com>
2016-10-22 23:37:04 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 echo
-echo "Hi from GitHub -Eric"
+echo "Hi from GitHub"
+echo
+echo "This Script will walk through some of the basic and tedious first steps with a new Raspberry Pi."
+echo "Let's start by doing some system software updates..."
 
 #let's get the current version of Raspbian
 current_ver=`uname -a`
 echo
-echo "you're running:"
+echo "Before we start, it looks like you're running:"
 echo "  $current_ver"
 echo
-
-#get some date and time info
-echo "the current time zone set is:"
-echo "  "
-
-#ask for a new password
+echo "  Installing updates, this may take some time..."
 echo
-echo "You will now be asked to enter a new password for the pi user"
-passwd
+sudo apt-get update -y
 echo
+sudo apt-get dist-upgrade -y
+echo
+
+echo
+echo "Now that updates are done, let's install some software:"
+echo "  Starting with Vim..."
+echo
+sudo apt-get install vim -y
+echo
+echo "  Now installing NGiNX..."
+echo
+sudo apt-get install nginx -y
+echo
+echo "  Now installing sSMTP..."
+echo
+sudo apt-get install ssmtp -y
+echo
+echo "  Now installing Telnet..."
+echo
+sudo apt-get install telnet -y
+echo
+echo "  Now installing Links..."
+echo
+sudo apt-get install links -y
+echo
+
+#now to do the basic stuff, we can use the raspberry pi config script 
